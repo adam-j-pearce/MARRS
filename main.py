dedab95a4177fcabb3f23485da57015ebb72075a
adam-j-pearce <120463633+adam-j-pearce@users.noreply.github.com>
2023-10-14 16:03:34 +0100

    need to reinstall

---
+++ main.py
@@ -0,0 +1,41 @@
+import irsdk
+import time
+
+class State:
+    ir_connected = False
+
+def check_iracing():
+    if state.ir_connected and not (ir.is_initialized and ir.is_connected):
+        state.ir_connected = False
+        # don't forget to reset your State variables
+        state.last_car_setup_tick = -1
+        # we are shutting down ir library (clearing all internal variables)
+        ir.shutdown()
+        print('irsdk disconnected')
+    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
+        state.ir_connected = True
+        print('irsdk connected')
+
+def loop():
+    print("hello world")
+
+if __name__ == '__main__':
+    # initializing ir and state
+    ir = irsdk.IRSDK()
+    state = State()
+
+    try:
+        # infinite loop
+        while True:
+            # check if we are connected to iracing
+            check_iracing()
+            # if we are, then process data
+            if state.ir_connected:
+                loop()
+            # sleep for 1 second
+            # maximum you can use is 1/60
+            # cause iracing updates data with 60 fps
+            time.sleep(1)
+    except KeyboardInterrupt:
+        # press ctrl+c to exit
+        pass
